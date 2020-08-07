93d379b1dc5a0cf758bf9045f2c5343f81b4b62d
MonicaQuezada <40504907+MonicaQuezada@users.noreply.github.com>
2020-08-07 04:53:19 -0500

    Update search.js

--- js/search.js
+++ js/search.js
@@ -1,8 +1,4 @@
-// After the API loads, call a function to enable the search box.
-function handleAPILoaded() {
-    $('#search-button').attr('disabled', false);
-  }  
-  // Search for a specified string.
+   // Search for a specified string.
   function search() {
     var q = $('#query').val();
     var request = gapi.client.youtube.search.list({
